b84e8df82c7f62b137e205b4efd3dd9d0a0a72de
AcrylicShrimp <led789zxpp@naver.com>
2019-02-22 22:10:21 +0900

    Minified the id generation function.

--- src/routes/helper.js
+++ src/routes/helper.js
@@ -5,11 +5,7 @@ const bcrypt = require('bcrypt');
 const crypto = require('crypto');
 
 module.exports = {
-	generateId     : () => new Promise((resolve, reject) => {
-		crypto.randomBytes(256, (err, buffer) => {
-			err ? reject(err): resolve(crypto.createHash('sha256').update(buffer).digest('hex'));
-		});
-	}),
+	generateId     : () => new Promise((resolve, reject) => crypto.randomBytes(256, (err, buffer) => err ? reject(err): resolve(crypto.createHash('sha256').update(buffer).digest('hex')))),
 	hashPassword   : password => bcrypt.hash(password, 15),
 	comparePassword: (password, hash) => bcrypt.compare(password, hash)
 };
