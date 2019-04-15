2f0b60acc39d2e068d853f6699e9e77cabf190da
AcrylicShrimp <led789zxpp@naver.com>
2019-04-15 11:54:02 +0900

    Modified the draft version.

--- src/acme-handler.js
+++ src/acme-handler.js
@@ -8,7 +8,7 @@ const path             = require('path');
 const config = require('../configs/config.json');
 
 module.exports = greenLockExpress.create({
-	version        : 'draft-11',
+	version        : 'draft-12',
 	server         : 'https://acme-v02.api.letsencrypt.org/directory',
 	email          : config['acme-email'],
 	approvedDomains: config['acme-domains'],
